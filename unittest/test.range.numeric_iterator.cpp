3892467cf4ab9e37be45569045fa5a7f48d83f7d
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2006-12-27 09:52:07 +0000

    Recommiting 26:30 [was clobbered by factory merge]
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/trunk@36 9ff9cde7-7a25-0410-a305-d194f5e702f5

--- unittest/test.range.numeric_iterator.cpp
+++ unittest/test.range.numeric_iterator.cpp
@@ -11,6 +11,39 @@
 #include <industry/iterator/numeric_iterator.hpp>
 #include <boost/test/unit_test.hpp>
 #include <vector>
+#include <industry/algorithm.hpp>
+#include <industry/math/fixed.hpp>
+#include <boost/test/floating_point_comparison.hpp>
 
-void test_range_numeric_iterator( void ) {
+void test_range_numeric_iterator( ) {
+	using namespace industry;
+	
+	typedef fixed<int, 6> fp6;
+
+	int expected_result[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int expected_result_odd[] = {1, 3, 5, 7, 9};
+
+	std::vector<int> results;
+	make_numeric_range(1, 10) | push_back(results);
+
+	BOOST_CHECK(( make_range(expected_result) == make_range(results) ));
+
+	results.clear();
+
+	make_numeric_range(1, 10, 2) | push_back(results);
+
+	BOOST_CHECK(( make_range(expected_result_odd) == make_range(results) ));
+
+	numeric_iterator<int> f(1, 3, 1);
+	++f;
+	++f;
+	//++f; - SK: asserts if you attempt to go past the end.
+
+	f = numeric_iterator<int>(1, 3, 3);
+	++f;
+	//++f; - SK: asserts if you attempt to go past the end.
+
+ 	std::vector<fp6> fp_results;
+	make_numeric_range(fp6(0.0), fp6(1.0), fp6(0.1)) | push_back(fp_results);
+	BOOST_CHECK(( fp_results.size() == 11 )); //Note fp6 representation will result in 11 elements, not 10.
 }
