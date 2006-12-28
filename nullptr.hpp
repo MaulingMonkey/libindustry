6d898f72a7c9a9966e5044611b4cd1356d41010f
WashuHibiki <WashuHibiki@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2006-12-28 07:30:38 +0000

    Added nullptr implementation.
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/trunk/industry@64 9ff9cde7-7a25-0410-a305-d194f5e702f5

---
+++ nullptr.hpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2006 Michael B. Edwin Rickert
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt )
+//
+// $LastChangedBy$ - $LastChangedDate$
+
+#ifndef IG_INDUSTRY_NULLPTR
+#define IG_INDUSTRY_NULLPTR
+
+namespace industry {
+	class nullptr_t {
+	public:
+		template<typename ValueT> operator ValueT*() const { return 0; }
+		template<typename ClassT, typename ValueT> operator ValueT ClassT::*() const { return 0; }
+	private:
+		void operator&() const;
+	};
+
+	const nullptr_t nullptr = {};
+}
+
+#endif //IG_INDUSTRY_NULLPTR
