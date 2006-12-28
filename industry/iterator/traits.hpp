151eb0e01faf6dacf2c03d3d8445410666bd2ea5
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2006-12-28 22:54:27 +0000

    [algorithm] Added INDUSTRY_TYPEOF(x) to industry/config.hpp for GCC
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/branches/algorithm@79 9ff9cde7-7a25-0410-a305-d194f5e702f5

--- industry/iterator/traits.hpp
+++ industry/iterator/traits.hpp
@@ -15,6 +15,7 @@
 #ifndef IG_INDUSTRY_ITERATOR_TRAITS
 #define IG_INDUSTRY_ITERATOR_TRAITS
 
+#include <industry/config.hpp>
 #include <industry/member_function_ptr_traits.hpp>
 #include <boost/type_traits/function_traits.hpp>
 #include <iterator>
@@ -29,7 +30,7 @@ namespace industry {
 		typedef typename industry::member_function_ptr_traits< typeof(&Iterator::operator* ) >::result_type   result_type;
 		typedef typename industry::member_function_ptr_traits< typeof(&Iterator::operator->) >::result_type   ptr_result_type;
 	};
-	
+		
 	template < typename ValueT >
 	struct iterator_traits< ValueT * > : iterator_traits_base< ValueT * > {
 		typedef ValueT & result_type;
