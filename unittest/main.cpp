3a683511ec66e738f71d0a36df92695b6479c11c
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2006-12-28 10:14:34 +0000

    [algorithm] R69 lied (missed unittest/test.iterator.traits.cpp) -- fixed
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/branches/algorithm@70 9ff9cde7-7a25-0410-a305-d194f5e702f5

--- unittest/main.cpp
+++ unittest/main.cpp
@@ -38,6 +38,7 @@ void test_factory();
 void test_inherit();
 void test_iterator_cast();
 void test_iterator_n();
+void test_iterator_traits();
 void test_math_fixed();
 void test_math_vector();
 void test_multitype();
@@ -55,6 +56,7 @@ static test_function_ptr test_functions[] = {
 	test_inherit,
 	test_iterator_cast,
 	test_iterator_n,
+	test_iterator_traits,
 	test_math_fixed,
 	test_math_vector,
 	test_multitype,
