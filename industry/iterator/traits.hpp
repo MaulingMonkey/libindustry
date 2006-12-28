2334af19e01ca25caecde2243d4f983239005d31
WashuHibiki <WashuHibiki@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2006-12-28 23:40:32 +0000

    Adjusted to just handle standard iterators
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/branches/algorithm@81 9ff9cde7-7a25-0410-a305-d194f5e702f5

--- industry/iterator/traits.hpp
+++ industry/iterator/traits.hpp
@@ -29,21 +29,14 @@
 
 namespace industry {
 	template < typename Iterator >
-	struct iterator_traits_base : std::iterator_traits< Iterator > {
-	};
+	struct iterator_traits_base : std::iterator_traits< Iterator > { };
 	
-#if defined( INDUSTRY_TYPEOF )
 	template < typename Iterator >
 	struct iterator_traits : iterator_traits_base< Iterator > {
-		typedef typename industry::member_function_ptr_traits< INDUSTRY_TYPEOF(&Iterator::operator* ) >::result_type   result_type;
-		typedef typename industry::member_function_ptr_traits< INDUSTRY_TYPEOF(&Iterator::operator->) >::result_type   ptr_result_type;
+		typedef typename std::iterator_traits<Iterator>::pointer ptr_result_type;
+		typedef typename std::iterator_traits<Iterator>::reference result_type;
 	};
-#else
-	template < typename Iterator > struct iterator_traits;
-	
-	//TODO: Specialize iterator_traits for standard library iterators
-#endif
-		
+
 	template < typename ValueT >
 	struct iterator_traits< ValueT * > : iterator_traits_base< ValueT * > {
 		typedef ValueT & result_type;
