da7e06a4e671ac96e17ba7fc3aac584ebee2ab09
WashuHibiki <WashuHibiki@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2006-12-29 00:50:07 +0000

    Reverted to transform with deffer's modifications (and fixes)
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/trunk/industry@90 9ff9cde7-7a25-0410-a305-d194f5e702f5

--- algorithm/transform.hpp
+++ algorithm/transform.hpp
@@ -5,6 +5,7 @@
 // http://www.boost.org/LICENSE_1_0.txt )
 //
 // $LastChangedBy$ - $LastChangedDate$
+// Adopted changes from deffer (GDNet) 
 
 #ifndef IG_INDUSTRY_ALGORITHM_TRANSFORM
 #define IG_INDUSTRY_ALGORITHM_TRANSFORM
@@ -18,47 +19,36 @@
 namespace industry {
 	namespace algorithm {
 		template< typename FunctionS, typename HeldIterator >
-		class transform_iterator_adaptor : public boost::iterator_facade<
-			transform_iterator_adaptor<FunctionS, HeldIterator>,
-			typename boost::remove_pointer<typename boost::function< FunctionS >::result_type>::type,
-			typename std::iterator_traits<HeldIterator>::iterator_category
-		> {
+		class transform_iterator_adaptor;
+
+		template < class FunctionS, class HeldIterator >
+		struct transform_iterator_adaptor_base
+		{
+			typedef boost::iterator_adaptor<
+				transform_iterator_adaptor< FunctionS, HeldIterator >
+				, HeldIterator
+				, typename boost::remove_reference< typename boost::function< FunctionS >::result_type >::type
+				, typename std::iterator_traits< HeldIterator >::iterator_category
+				, typename boost::function< FunctionS >::result_type
+			> type;
+		};
+
+		template< typename FunctionS, typename HeldIterator >
+		class transform_iterator_adaptor : public transform_iterator_adaptor_base<FunctionS, HeldIterator>::type
+		{
 			typedef boost::function< FunctionS > function_type;
+			typedef typename boost::function< FunctionS >::result_type dereferenced_type;
+			typedef typename transform_iterator_adaptor_base< FunctionS, HeldIterator >::type base_type;
 		public:
-			transform_iterator_adaptor(function_type function, HeldIterator itor) : function(function), itor(itor){ }
-			typedef typename std::iterator_traits<HeldIterator>::difference_type difference_type;
+			transform_iterator_adaptor(function_type function, HeldIterator itor) : base_type(itor), function(function) { }
 		private:
 			friend class boost::iterator_core_access;
 
-			template <typename FunctionOther, typename HeldItorOther>
-			bool equal(transform_iterator_adaptor<FunctionOther, HeldItorOther> const& other) const
-			{ 
-				return this->itor == other.itor;
-			}
-
-			void increment() {
-				++itor;
-			}
-
-			void decrement() {
-				--itor;
-			}
-
-			void advance(difference_type n) {
-				itor += n;
-			}
-
-			template <typename FunctionOther, typename HeldItorOther>
-			difference_type distance_to(transform_iterator_adaptor<FunctionOther, HeldItorOther> const& other) const {
-				return std::distance(itor, other.itor);
-			}
-
-			typename boost::remove_pointer<typename function_type::result_type>::type& dereference() const {
-				return *function(&*itor);
+			dereferenced_type dereference() const {
+				return function(*this->base_reference());
 			}
 
 			function_type function;
-			HeldIterator itor;
 		};
 
 		template< typename FunctionS >
@@ -89,14 +79,14 @@ namespace industry {
 			return transform_proxy<R (A1)>(function);
 		}
 
-		template< typename R, typename C >
-		transform_proxy<R (C*)> transform(R (C::*function)()) {
-			return transform_proxy<R (C*)>(function);
-		}
-
 		template< typename R, typename A1 >
 		call_processor< R (A1) > transform( boost::function<R (A1)> function) {
 			return transform_proxy< R (A1) >(function);
+		}
+
+		template< typename R, typename C >
+		transform_proxy<R (C&)> transform(R (C::*function)()) {
+			return transform_proxy<R (C&)>(function);
 		}
 	}
 
