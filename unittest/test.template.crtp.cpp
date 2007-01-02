fc6afbb53ba4902d16ff6a1299da6d8bde6fcc44
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2007-01-02 09:50:58 +0000

    [trunk] Added industry/templates/crtp.hpp
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/trunk@158 9ff9cde7-7a25-0410-a305-d194f5e702f5

---
+++ unittest/test.template.crtp.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) 2007 Michael B. Edwin Rickert
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt )
+//
+// Jan  2, 2007 - Created
+// $LastChangedBy$ - $LastChangedDate$
+
+#include <industry/template/crtp.hpp>
+#include <boost/test/unit_test.hpp>
+
+namespace {
+	template < typename Self > struct class1 {};
+	template < typename Self , typename , typename > struct class2 {};
+	template < typename Self , typename > struct class3 {};
+	
+	using namespace ::industry::templates;
+	class user : public crtp_inherit< user , crtp<class1> , crtp2<class2,int,char> , crtp1<class3,bool> > { public: virtual ~user() {} };
+}
+
+void test_template_crtp() {
+	user example;
+	
+	BOOST_CHECK((  dynamic_cast< class1<user>          * >( (user*) &example ) ));
+	BOOST_CHECK((  dynamic_cast< class2<user,int,char> * >( (user*) &example ) ));
+	BOOST_CHECK((  dynamic_cast< class3<user,bool>     * >( (user*) &example ) ));
+	BOOST_CHECK(( !dynamic_cast< ::industry::nil       * >( (user*) &example ) ));
+}
