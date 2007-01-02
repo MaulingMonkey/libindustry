81a2f9b6d583cc7bb9023b824f3f06cb64dbf9b9
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2007-01-02 08:26:15 +0000

    [trunk] Generalization of templates::tt1 to ttN
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/trunk@156 9ff9cde7-7a25-0410-a305-d194f5e702f5

--- industry/template/tt.hpp
+++ industry/template/tt.hpp
@@ -6,9 +6,20 @@
 //
 // Jan  1, 2007 - Created
 
+#ifndef INDUSTRY_TEMPLATE_TT_LIMIT
+#define INDUSTRY_TEMPLATE_TT_LIMIT 10
+#endif //ndef INDUSTRY_TEMPLATE_TT_LIMIT
+
 namespace industry {
 	namespace templates {
-		template < template < typename > class class_ > struct tt1 { template < typename T1 > struct with { typedef class_< T1 > type; }; };
+		#define IMPL_TTn(z,n,unused)                                                                                       \
+		template < template < BOOST_PP_ENUM_PARAMS( n , typename BOOST_PP_INTERCEPT ) > class class_ > struct tt ## n {    \
+			template < BOOST_PP_ENUM_PARAMS( n , typename T ) > struct with {                                              \
+				typedef class_< BOOST_PP_ENUM_PARAMS( n , T ) > type;                                                      \
+			};                                                                                                             \
+		}; /*-------------------------------------------------------------------------------------------------------------*/
+		BOOST_PP_REPEAT_FROM_TO( 1 , BOOST_PP_ADD( 1 , INDUSTRY_TEMPLATE_TT_LIMIT ) , IMPL_TTn , ~ )
+		#undef IMPL_TTn
 	}
 	using ::industry::templates::tt1;
 }
