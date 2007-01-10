5f4e2eff5077bb8fc7f923e5df7c13217418f09f
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2007-01-10 07:12:22 +0000

    [panda-api] windows\[registry\]config.hpp
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/branches/panda-api@210 9ff9cde7-7a25-0410-a305-d194f5e702f5

---
+++ industry/api/windows/config.hpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2006 Michael B. Edwin Rickert
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt )
+//
+// Jan  9, 2007 - Added to SVN, cleaned up
+// Nov 24, 2006 - Created
+
+#ifndef IG_INDUSTRY_API_WINDOWS_CONFIG
+#define IG_INDUSTRY_API_WINDOWS_CONFIG
+
+#include <industry/config.hpp>
+
+#if !defined( INDUSTRY_OS_WINDOWS )
+#error "INDUSTRY_OS_WINDOWS required for windows"
+#endif
+
+#endif //ndef IG_INDUSTRY_API_WINDOWS_CONFIG
---
+++ industry/api/windows/registry/config.hpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2006 Michael B. Edwin Rickert
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt )
+//
+// Jan  9, 2007 - Added to SVN, cleaned up
+// Oct 26, 2006 - "win32" replaced with "windows" - should be mostly architecture independant
+// Aug 17, 2006 - Created
+
+#ifndef IG_INDUSTRY_API_WINDOWS_REGISTRY_CONFIG
+#define IG_INDUSTRY_API_WINDOWS_REGISTRY_CONFIG
+
+#include <industry/windows/config.hpp>
+
+#endif //ndef IG_INDUSTRY_API_WINDOWS_REGISTRY_CONFIG
