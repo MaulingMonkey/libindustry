0502b6855630e561d1e98cbc5c9a8f5c914bef60
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2007-01-25 00:21:28 +0000

    [trunk] Inroads to algorithm processor support -- eliminated some dead code from code that will be dead itself soon.
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/trunk@286 9ff9cde7-7a25-0410-a305-d194f5e702f5

--- industry/algorithm/concepts.hpp
+++ industry/algorithm/concepts.hpp
@@ -9,8 +9,29 @@
 #ifndef IG_INDUSTRY_ALGORITHM_CONCEPTS
 #define IG_INDUSTRY_ALGORITHM_CONCEPTS
 
+#include <industry/range.hpp>
+
 namespace industry {
 	namespace algorithm {
+		template < typename BoundProcessor > struct active_processor_of { typedef typename BoundProcessor::active_processor_type type; };
+		template < typename Iterator > class active_range_processor {
+			      Iterator i;
+			const Iterator end_i;
+		public:
+			typedef typename industry::range<Iterator>::reference reference;
+			active_range_processor( industry::range<Iterator> const& r ): i(r.begin()), end_i(r.end()) {}
+
+			reference get() const { return *i;} 
+			void advance() { ++i; }
+			bool end() { return i == end_i; }
+		};
+		template < typename Iterator > struct active_processor_of< industry::range< Iterator > > { typedef active_range_processor<Iterator> type; };
+
+		//template < typename ActiveProccessorT , typename NextUnboundProcessor = industry::nil > struct unbound_processor {
+		//	friend ... activate( unbound_processor const& p ) {
+		//};
+
+		//template < typename BoundProcessorConvertable > struct bound_processor_of {};
 		/* Primary concepts:
 		 *
 		 * - UnboundProcessor
