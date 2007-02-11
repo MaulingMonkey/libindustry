02e3caa01da7978d8bb4bcc9e2d948f2d5305693
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2007-02-11 06:14:52 +0000

    [build] #comments!  And dependancies for Target
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/branches/build@328 9ff9cde7-7a25-0410-a305-d194f5e702f5

--- build/target.rb
+++ build/target.rb
@@ -8,8 +8,18 @@
 
 module Industry
 	class Target
-		attr_accessor :name  #TODO: Add name validity checking
+		# TODO: Add name validity checking
+		attr_accessor :name
 
+
+		# Conceptual target dependancies (e.g. program depends on source files, not
+		# the object files, which are a toolchain implementation detail)
+		attr_reader   :dependancies
+
+
+		# Conceptual target type (e.g. program, static lib, or dynamic lib, not the OS
+		# specific renditions of these, of which multiple may exist (.so and .dylib on
+		# OS X for example)
 		attr_reader   :type
 		def type=( value )
 			case value
@@ -28,6 +38,8 @@ module Industry
 			else
 				raise ArgumentError , "Expected [0..1] arguments, got #{args.size}"
 			end
+
+			@dependancies = []
 		end
 	end
 end
