56ff4de59e6dd5adfe265491fbc5677b68aa2fc4
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2007-02-15 09:40:06 +0000

    [build] More fleshing out
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/branches/build@333 9ff9cde7-7a25-0410-a305-d194f5e702f5

--- build.rb
+++ build.rb
@@ -1,34 +1,41 @@
-def target(hash)
+def target(name, &block)
+	block.call()
 end
+def targets(*names, &block)
+	names.each do |name|
+		target(name,&block)
+	end
+end
+def type(name); end
+def dependancies(*list); end
+def filename(name); end
 
-target {
-	:name => "libindustry",
-	:type => "metatarget",
-	:dependancies =>
-		[ "libindustry-static"
-		, "libindustry-dynamic"
-		, "libindustry-unittest"
-		]
-	}
+target("libindustry") {
+	type    :meta
 
-target {
-	:name => "libindustry-static",
-	:type => :staticlib,
-	:filename => case $OS
+	dependancies "libindustry-static", "libindustry-dynamic", "libindustry-unittest"
+}
+
+target("libindustry-static") {
+	type    :staticlib
+	filename case $OS
 		when "Windows"; "libindustry.lib"
 		when "Linux";   "libindustry.a"
 		when "OS X";    "libindustry.a"
 		else;           nil
 	end
-	}
+}
 
-target {
-	:name => "libindustry-dynamic",
-	:type => :dynamiclib,
-	:filename => case $OS
+target("libindustry-dynamic") {
+	type    :dynamiclib
+	filename case $OS
 		when "Windows"; "libindustry.dll"
 		when "Linux";   "libindustry.so"
 		when "OS X";    "libindustry.dylib"
 		else;           nil
 	end
-	}
+}
+
+targets("libindustry-static","libindustry-dynamic") {
+	dependancies "ruby"
+}
