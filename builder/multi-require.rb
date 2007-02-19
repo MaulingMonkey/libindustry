d681438b191815e42c15a6e7e01543115aa3b7f4
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2007-02-19 14:34:44 +0000

    [build] Added builder\multi-require.rb (pattern require statements, etc)
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/branches/build@343 9ff9cde7-7a25-0410-a305-d194f5e702f5

---
+++ builder/multi-require.rb
@@ -0,0 +1,31 @@
+# Copyright (c) 2006-2007 Michael B. Edwin Rickert
+# 
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt )
+# 
+# $LastChangedBy$
+# $LastChangedDate$
+
+def require( *patterns )
+	results = patterns.collect do |pattern|
+		paths = Dir[pattern.gsub(/\\/,'/')]
+		if paths.empty? then
+			super pattern
+		else
+			successful = false
+			statuses = paths.collect do |path|
+				begin
+					result = super path
+					successful = true
+					result
+				rescue LoadError
+					nil #assumed to be a directory or the like
+				end
+			end
+			raise LoadError, "no such pattern to match/load -- #{pattern}" unless successful
+			return *statuses.compact
+		end
+	end
+	return *results
+end
