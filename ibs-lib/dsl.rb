00f8b4891e596af4fee083f8fd47d0d02881e695
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2007-04-04 04:47:11 +0000

    [build] Added copyright/license comment notices to ibs-lib\*.rb (meant to add those before committing originally)
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/branches/build@387 9ff9cde7-7a25-0410-a305-d194f5e702f5

--- ibs-lib/dsl.rb
+++ ibs-lib/dsl.rb
@@ -1,3 +1,11 @@
+#  Copyright (c) 2007 Michael B. Edwin Rickert
+# 
+#  Distributed under the Boost Software License, Version 1.0.
+#  (See accompanying file LICENSE_1_0.txt or copy at
+#  http://www.boost.org/LICENSE_1_0.txt )
+# 
+#  $LastChangedBy$ - $LastChangedDate$
+
 def add_include_paths( list )
 	raise ArgumentError , "Must be used in the context of a dependancy, program, or library" unless ($focus && $focus.respond_to?('include_paths='))
 	
