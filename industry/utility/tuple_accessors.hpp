c4de9fb06048da45fca6c70f8a437684f82529e7
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2007-04-16 14:49:22 +0000

    [utility]  get_at_type implementation & tests
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/branches/utility@425 9ff9cde7-7a25-0410-a305-d194f5e702f5

--- industry/utility/tuple_accessors.hpp
+++ industry/utility/tuple_accessors.hpp
@@ -10,8 +10,12 @@
 #define IG_INDUSTRY_UTILITY_TUPLE_ACCESSORS
 
 #include <industry/sfinae.hpp>
+#include <industry/nil.hpp>
 #include <boost/mpl/eval_if.hpp>
 #include <boost/mpl/identity.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <boost/type_traits.hpp>
 
 namespace industry {
 	namespace utility {
@@ -20,6 +24,59 @@ namespace industry {
 		// T get_at_type_or_default< T >   ( Tuple , default )
 		// T get_if_type           < Pred >( Tuple )
 		// T get_if_type_or_default< Pred >( Tuple , default )
+
+		namespace detail {
+			template < typename Tuple, typename T > struct tuple_has_type {
+				enum { value
+					=  boost::is_same< typename Tuple::head_type , T >::value
+					|| tuple_has_type< typename Tuple::tail_type , T >::value
+				};
+			};
+			template < typename Tuple, typename T > struct tuple_has_many_of_type {
+				enum { value
+					=  boost::is_same< typename Tuple::head_type , T >::value
+					?  tuple_has_type< typename Tuple::tail_type , T >::value
+					:  tuple_has_many_of_type< typename Tuple::tail_type , T >::value
+				};
+			};
+			template < typename Tuple, typename T > struct tuple_has_one_of_type {
+				enum { value
+					=  boost::is_same< typename Tuple::head_type , T >::value
+					? !tuple_has_type< typename Tuple::tail_type , T >::value
+					:  tuple_has_one_of_type< typename Tuple::tail_type , T >::value
+				};
+			};
+			template < typename T > struct tuple_has_type        < boost::tuples::null_type , T > { enum { value = false }; };
+			template < typename T > struct tuple_has_many_of_type< boost::tuples::null_type , T > { enum { value = false }; };
+			template < typename T > struct tuple_has_one_of_type < boost::tuples::null_type , T > { enum { value = false }; };
+
+			template < typename T , typename Tuple , bool equal = boost::is_same< T , typename Tuple::head_type >::value > struct tuple_get_at {
+				static T from( const Tuple& tuple ) {
+					return tuple_get_at< T , typename Tuple::tail_type >::from( tuple.get_tail() );
+				}
+				static T from( const Tuple& tuple, const T& default_ ) {
+					return tuple_get_at< T , typename Tuple::tail_type >::from( tuple.get_tail(), default_ );
+				}
+			};
+
+			template < typename T , typename Tuple > struct tuple_get_at< T , Tuple , true > {
+				static T from( const Tuple& tuple )                        { return tuple.get_head(); }
+				static T from( const Tuple& tuple, const T& /*default_*/ ) { return tuple.get_head(); }
+			};
+
+			template < typename T > struct tuple_get_at< T , boost::tuples::null_type , false > {
+				static T from( const boost::tuples::null_type& /*tuple*/, const T& default_ ) { return default_; }
+			};
+		}
+
+		template < typename T , typename Tuple > T get_at_type( const Tuple& tuple ) {
+			BOOST_STATIC_ASSERT(( detail::tuple_has_one_of_type< Tuple , T >::value ));
+			return detail::tuple_get_at<T,Tuple>::from(tuple);
+		}
+		template < typename T , typename Tuple > T get_at_type_or_default( const Tuple& tuple, const T& default_ ) {
+			BOOST_STATIC_ASSERT(( !detail::tuple_has_many_of_type< Tuple , T >::value ));
+			return detail::tuple_get_at<T,Tuple>::from(tuple,default_);
+		}
 	}
 }
 
--- projects/test.utility.cpp
+++ projects/test.utility.cpp
@@ -6,6 +6,7 @@
 //
 // $LastChangedBy$ - $LastChangedDate$
 
+#include <industry/utility/tuple_accessors.hpp>
 #include <boost/test/unit_test.hpp>
 #include <boost/tuple/tuple.hpp>
 
@@ -13,4 +14,20 @@ namespace {
 }
 
 void test_utility( void ) {
+	using namespace industry::utility;
+	boost::tuple< char, int, float, short > legal1( 'a', 42, 3.1415f , 1 );
+	BOOST_CHECK_EQUAL( legal1.get<0>() , get_at_type< char  >( legal1 ) );
+	BOOST_CHECK_EQUAL( legal1.get<1>() , get_at_type< int   >( legal1 ) );
+	BOOST_CHECK_EQUAL( legal1.get<3>() , get_at_type< short >( legal1 ) );
+	//get_at_type< double >( legal1 ); // OK (clean error)
+
+
+	boost::tuple< char, char >      reoccurance1( 'a', 'a' );
+	boost::tuple< char, int, char > reoccurance2( 'a', 42, 'a' );
+	boost::tuple< int, char, char > reoccurance3( 42, 'a', 'a' );
+	BOOST_CHECK_EQUAL( reoccurance2.get<1>() , get_at_type< int >( reoccurance2 ) );
+	BOOST_CHECK_EQUAL( reoccurance2.get<0>() , get_at_type< int >( reoccurance3 ) );
+	//get_at_type< char >( reoccurance1 ); //OK (clean error)
+	//get_at_type< char >( reoccurance2 ); //OK (clean error)
+	//get_at_type< char >( reoccurance3 ); //OK (clean error)
 }
