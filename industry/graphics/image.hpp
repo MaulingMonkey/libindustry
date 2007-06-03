2174257d6ec75e1032743fae4ae35de6617057b6
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2007-06-03 07:24:08 +0000

    [graphics]  Removed stealing constructors -- no multi_array swap func to take advantage of
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/branches/graphics@482 9ff9cde7-7a25-0410-a305-d194f5e702f5

--- industry/graphics/image.hpp
+++ industry/graphics/image.hpp
@@ -23,7 +23,7 @@ namespace industry {
 		private:
 			typedef          boost::multi_array<ColorT,2>                     data_type;
 			typedef typename boost::multi_array<ColorT,2>::size_type          index_type;
-			data_type  data;
+			data_type  data_;
 
 
 		public:
@@ -32,15 +32,15 @@ namespace industry {
 				index_type row, column;
 			public:
 				reference( image* self, index_type row, index_type column ): self(self), row(row), column(column) {}
-				operator color_type() const { return self->data[column][row]; }
-				reference& operator=( const color_type& color ) { self->data[column][row] = color; return *this; }
+				operator color_type() const { return self->data_[column][row]; }
+				reference& operator=( const color_type& color ) { self->data_[column][row] = color; return *this; }
 			};
 			class const_reference {
 				const image* self;
 				index_type row, column;
 			public:
 				const_reference( const image* self, index_type row, index_type column ): self(self), row(row), column(column) {}
-				operator color_type() const { return self->data[column][row]; }
+				operator color_type() const { return self->data_[column][row]; }
 			};
 			template < typename ImageT, typename ReferenceT >
 			class slice {
@@ -53,32 +53,30 @@ namespace industry {
 
 
 		public:
-			image(): data() {}
-			image( index_type width, index_type height ): data(boost::extents[width][height]) {}
-			image( std::auto_ptr< boost::multi_array<ColorT,2> > stolen_data ) {
-				data.swap(stolen_data); // TODO:  Test
-			}
-			image( std::auto_ptr< image > stolen_image ) {
-				data.swap(stolen_image->data); // TODO:  Test
-			}
-			image( const image& other ): data(other.data) {}
+			image(): data_() {}
+			image( index_type width, index_type height ): data_(boost::extents[width][height],boost::fortran_storage_order()) {}
+			image( const image& other ): data_(other.data) {}
 
 			template < typename OColorT > friend class image;
 			template < typename OColorT >
-			image( const image<OColorT>& other ): data(boost::extents[other.width()][other.height()]) {
+			image( const image<OColorT>& other ): data_(boost::extents[other.width()][other.height()]) {
 				for ( unsigned y = 0 ; y < other.height() ; ++y ) {
 					for ( unsigned x = 0 ; x < other.width() ; ++x ) {
-						data[x][y] = color_type( other[x][y] );
+						data_[x][y] = color_type( other[x][y] );
 					}
 				}
 			}
 
-
 			slice< image      , reference       > operator[]( index_type  column )       { return slice< image      , reference       >(this,column); }
 			slice< const image, const_reference > operator[]( index_type  column ) const { return slice< const image, const_reference >(this,column); }
-			index_type width() const { return data.shape()[0]; }
-			index_type height() const { return data.shape()[1]; }
-			void resize( index_type width, index_type height ) { data.resize( boost::extents[width][height] ); }
+
+			index_type width()  const { return data_.shape()[0]; }
+			index_type height() const { return data_.shape()[1]; }
+			void resize( index_type width, index_type height ) { data_.resize( boost::extents[width][height] ); }
+
+			const index_type data_size() const { return width()*height(); }
+			      ColorT* data()       { return data_.data(); }
+			const ColorT* data() const { return data_.data(); }
 		};
 	}
 }
