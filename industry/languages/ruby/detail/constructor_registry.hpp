32ac8986ae2f3cb6abfde668b1be12f54995565c
washuhibiki <washuhibiki@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2008-05-15 04:28:58 +0000

    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/trunk@564 9ff9cde7-7a25-0410-a305-d194f5e702f5

---
+++ industry/languages/ruby/detail/constructor_registry.hpp
@@ -0,0 +1,76 @@
+// Copyright (c) 2007-2008 Sean Kent
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt )
+//
+// $LastChangedBy: washuhibiki $ - $LastChangedDate: 2008-05-13 13:45:12 -0700 (Tue, 13 May 2008) $
+
+#if !BOOST_PP_IS_ITERATING
+#ifndef IG_INDUSTRY_LANGAUGES_RUBY_DETAIL_CONSTRUCTOR_REGISTRY
+#define IG_INDUSTRY_LANGAUGES_RUBY_DETAIL_CONSTRUCTOR_REGISTRY
+
+#include <boost/preprocessor/repetition.hpp>
+#include <boost/preprocessor/iteration/iterate.hpp>
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/facilities/empty.hpp>
+#include <boost/preprocessor/list/at.hpp>
+#include <boost/preprocessor/list/for_each_product.hpp>
+#include <boost/preprocessor/tuple/elem.hpp>
+#include <boost/preprocessor/tuple/to_list.hpp>
+#include <boost/type_traits.hpp>
+#include <string>
+
+#ifdef _MSC_VER
+#pragma warning( push )
+#pragma warning( disable: 4100 ) // unreferenced formal parameters
+#endif
+
+namespace industry { namespace languages { namespace ruby { namespace detail {
+	template<class T, unsigned int A>
+	struct constructor_registry;
+
+	template<class T, class Sig, unsigned int A>
+	struct constructor_registry_impl {
+	};
+
+#	define BOOST_PP_ITERATION_LIMITS (0, 17)
+#	define BOOST_PP_FILENAME_1       <industry/languages/ruby/detail/constructor_registry.hpp>
+#	include BOOST_PP_ITERATE()
+
+}}}}
+#endif//IG_INDUSTRY_LANGAUGES_RUBY_DETAIL_CONSTRUCTOR_REGISTRY
+#else
+#define n BOOST_PP_ITERATION()
+#define FUNCTION_ARG_TYPE(i) BOOST_PP_CAT(typename func_type::arg, BOOST_PP_CAT(BOOST_PP_INC(i),_type))
+#define DO_GEN_KEYPART(x,i,d) (key += (rb_class2name((klass = rb_class_of(ruby_value<FUNCTION_ARG_TYPE(i)>::to(FUNCTION_ARG_TYPE(i)()))) == rb_cFixnum ? rb_cBignum : klass)))
+#define DO_WRAP_FUNC(z,i,data) (ruby_value<FUNCTION_ARG_TYPE(i)>::from(*(p++)))
+
+template<class T, class Sig>
+struct constructor_registry_impl<T, Sig, n> {
+	typedef boost::function<Sig> func_type;
+	static void reg() {
+		std::map<std::string, VALUE(*)(int, VALUE*, VALUE)>& m = constructor_registry<T, n>::get_constructors();
+		m[key()] = f;
+	}
+	
+	static std::string key() {
+		std::string key;
+		VALUE klass;
+		(BOOST_PP_ENUM(n, DO_GEN_KEYPART, BOOST_PP_EMPTY));
+		return key;
+	}
+	static VALUE f(int argc, VALUE* argv, VALUE self) {
+		VALUE* p = argv;
+
+		new (ruby_value<T*>::from(self)) T(BOOST_PP_ENUM(n, DO_WRAP_FUNC, BOOST_PP_EMPTY));
+
+		return self;
+	}
+};
+
+#undef n
+#undef DO_WRAP_VALUE
+#undef DO_WRAP_VALUE_MEMBER
+#undef FUNCTION_ARG_TYPE
+#endif
