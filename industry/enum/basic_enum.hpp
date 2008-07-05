24f1176c40de9d746e764f74c3a87cd2708aba09
pandamojo <pandamojo@9ff9cde7-7a25-0410-a305-d194f5e702f5>
2008-07-05 07:55:00 +0000

    [trunk]  Refactored out the trailing "count" enum entry for the raw INDUSTRY_ENUM(T,...)'s T::_ enumeration on the basis that this may help avoid superflous "not all paths return" and similar such warnings.
    git-svn-id: http://libindustry.svn.sourceforge.net/svnroot/libindustry/trunk@614 9ff9cde7-7a25-0410-a305-d194f5e702f5

--- industry/enum/basic_enum.hpp
+++ industry/enum/basic_enum.hpp
@@ -9,26 +9,27 @@
 #ifndef IG_INDUSTRY_ENUM_BASIC_ENUM
 #define IG_INDUSTRY_ENUM_BASIC_ENUM
 
+#include <boost/preprocessor/punctuation/comma_if.hpp>
 #include <boost/preprocessor/seq/enum.hpp>
-#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/seq/for_each_i.hpp>
 #include <boost/preprocessor/seq/seq.hpp>
 
-#define INDUSTRY_IMPL_ENUM_RAW_FILTER( r, unused_data, e ) BOOST_PP_CAT(_,e),
+#define INDUSTRY_IMPL_ENUM_RAW_FILTER( r, unused_data, i, e ) BOOST_PP_COMMA_IF(i) BOOST_PP_CAT(_,e)
 #define INDUSTRY_IMPL_ENUM_F( r, unused_data, e ) static void e( enumeration<BOOST_PP_CAT(_,e)> ) {}
 
 ////////////////////////////////////////////////////////////////////
 #define INDUSTRY_BASIC_ENUM( T, values )                           \
 class T {                                                          \
 public:                                                            \
-	enum _ { BOOST_PP_SEQ_FOR_EACH( INDUSTRY_IMPL_ENUM_RAW_FILTER, ~, values ) count }; \
-	_ value;                                                       \
+    enum _ { BOOST_PP_SEQ_FOR_EACH_I( INDUSTRY_IMPL_ENUM_RAW_FILTER, ~, values ) }; \
+    _ value;                                                       \
 private:                                                           \
-	template < _ > class enumeration {};                           \
+    template < _ > class enumeration {};                           \
 public:                                                            \
-	BOOST_PP_SEQ_FOR_EACH( INDUSTRY_IMPL_ENUM_F, ~, values )       \
+    BOOST_PP_SEQ_FOR_EACH( INDUSTRY_IMPL_ENUM_F, ~, values )       \
                                                                    \
     T(): value( BOOST_PP_CAT(_,BOOST_PP_SEQ_HEAD(values)) ) {}     \
-	template < _ value > T( void (*)( enumeration<value> ) ): value(value) {} \
+    template < _ value > T( void (*)( enumeration<value> ) ): value(value) {} \
     T( _ value ): value(value) {}                                  \
 };                                                                 \
 bool operator==( T lhs, T rhs ) { return lhs.value == rhs.value; } \
